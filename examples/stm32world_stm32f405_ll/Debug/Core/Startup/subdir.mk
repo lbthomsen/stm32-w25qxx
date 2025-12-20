6e5dbebe09083f54ef7a88c964b68a2db67e007d
Lars Boegild Thomsen <lth@netcompartner.com>
2025-12-21 06:57:00 +0800

    Removed one troublesome limitation from the license

---
+++ examples/stm32world_stm32f405_ll/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32f405rgtx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32f405rgtx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32f405rgtx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"/home/lth/src/stm32-w25qxx/examples/stm32world_stm32f405_ll/w25qxx" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Core-2f-Startup
+
+clean-Core-2f-Startup:
+	-$(RM) ./Core/Startup/startup_stm32f405rgtx.d ./Core/Startup/startup_stm32f405rgtx.o
+
+.PHONY: clean-Core-2f-Startup
+
